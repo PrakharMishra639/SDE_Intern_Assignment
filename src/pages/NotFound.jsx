0007630f2c0b21e2e1866ca57d02c80b07d5794e
PrakharMishra639 <mprakhar713@gmail.com>
2025-10-28 22:18:45 +0530

    updated code

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Button } from "@/components/ui/button";
+import { Button } from "../components/ui/button";
 import { Home, SearchX } from "lucide-react";
 
 export default function NotFound() {
